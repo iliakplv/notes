0bd8799fba615a2fa08da84a11161bf4b6df0f10
ilya <iliakplv@gmail.com>
2013-08-11 19:44:21 +0400

    project created

---
+++ src/com/iliakplv/notes/gui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iliakplv.notes.gui.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.iliakplv.notes.R;
+
+public class MainActivity extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
