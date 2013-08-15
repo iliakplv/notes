2bb993b01bcfd82306659906d5bc1016e5460f14
ilya <iliakplv@gmail.com>
2013-08-16 00:40:55 +0400

    Main screen UI draft

---
+++ src/com/iliakplv/notes/gui/main/NoteDetailsFragment.java
@@ -0,0 +1,28 @@
+package com.iliakplv.notes.gui.main;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.iliakplv.notes.R;
+
+public class NoteDetailsFragment extends Fragment {
+
+	private String text;
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		final View view = inflater.inflate(R.layout.note_details, container, false);
+		((TextView) view.findViewById(R.id.test_text)).setText(text);
+		return view;
+	}
+
+
+}
