c93149131f8a9c685a22347ff299a1ede0e61d75
ilya <iliakplv@gmail.com>
2013-08-22 01:20:08 +0400

    utils is final

--- src/com/iliakplv/notes/utils/Utils.java
+++ src/com/iliakplv/notes/utils/Utils.java
@@ -4,7 +4,7 @@ package com.iliakplv.notes.utils;
  * Author: Ilya Kopylov
  * Date:  21.08.2013
  */
-public class Utils {
+public final class Utils {
 
 	public static final boolean DEBUG = true;
 
