2447eba5c1eb9b31037ebc467348ec23724cf7a3
iliakplv <iliakplv@gmail.com>
2013-11-20 17:45:31 +0400

    [pre] settings activity

---
+++ app/src/main/java/com/iliakplv/notes/gui/settings/NotesSettingsActivity.java
@@ -0,0 +1,10 @@
+package com.iliakplv.notes.gui.settings;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Author: Ilya Kopylov
+ * Date: 20.11.2013
+ */
+public class NotesSettingsActivity extends PreferenceActivity {
+}
