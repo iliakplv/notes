f9daf05a671162c67b3c5f62c83e612da9fad26c
iliakplv <iliakplv@gmail.com>
2014-02-03 14:09:56 +0400

    label colors

---
+++ app/src/main/java/com/iliakplv/notes/notes/LabelColor.java
@@ -0,0 +1,14 @@
+package com.iliakplv.notes.notes;
+
+public class LabelColor {
+
+	public static final int NO_COLOR = 0; // transparent
+	public static final int RED = 0xFFe14f4f;
+	public static final int ORANGE = 0xFFd24726;
+	public static final int YELLOW = 0xFFf0ad4e;
+	public static final int GREEN = 0xFF8cc63f;
+	public static final int BLUE_LIGHT = 0xFF39b3d7;
+	public static final int BLUE_DARK = 0xFF3d6c87;
+	public static final int PURPLE = 0xFF811a56;
+
+}
