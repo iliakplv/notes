8fd08bb1a585747b279bb84cfafea0f80667c925
ilya <iliakplv@gmail.com>
2014-07-30 00:22:23 +0400

    trim search query string

--- app/src/main/java/com/iliakplv/notes/gui/main/MainActivity.java
+++ app/src/main/java/com/iliakplv/notes/gui/main/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends Activity implements NavigationDrawerFragment.N
 
 	private void performSearch(String searchQuery) {
 		if (!StringUtils.isBlank(searchQuery)) {
-			this.searchQuery = searchQuery;
+			this.searchQuery = searchQuery.trim();
 			updateUi();
 		}
 	}
