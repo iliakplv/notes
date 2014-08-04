9efa729b637b27f3e301c9737bf880891afcb7c3
ilya <iliakplv@gmail.com>
2014-08-05 02:41:16 +0400

    about - feedback

--- app/src/main/java/com/iliakplv/notes/gui/main/dialogs/AboutDialog.java
+++ app/src/main/java/com/iliakplv/notes/gui/main/dialogs/AboutDialog.java
@@ -5,7 +5,8 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.content.Context;
-import android.content.pm.PackageManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.util.Linkify;
 import android.view.LayoutInflater;
@@ -13,7 +14,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.iliakplv.notes.R;
-import com.iliakplv.notes.utils.AppLog;
+import com.iliakplv.notes.utils.Utils;
 
 public class AboutDialog extends DialogFragment {
 	private static final String FRAGMENT_TAG = AboutDialog.class.getSimpleName();
@@ -32,24 +33,37 @@ public class AboutDialog extends DialogFragment {
 		final LayoutInflater inflater = getActivity().getLayoutInflater();
 		final View view = inflater.inflate(R.layout.about_dialog, null);
 
-		String appVersion = "";
-		try {
-			appVersion = context.getPackageManager()
-					.getPackageInfo(context.getPackageName(), 0).versionName;
-		} catch (PackageManager.NameNotFoundException e) {
-			AppLog.wtf("About", "Can't get app version", e);
-		}
-
+		// version
+		final String appVersion = Utils.getVersionName();
 		((TextView) view.findViewById(R.id.version))
 				.setText(context.getString(R.string.about_version, appVersion));
 
-		final TextView contactInfo = (TextView) view.findViewById(R.id.contact);
-		Linkify.addLinks(contactInfo, Linkify.EMAIL_ADDRESSES);
+		// email
+		view.findViewById(R.id.contact).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				sendFeedback(context, appVersion);
+			}
+		});
 
+		// sources
 		final TextView sourcesInfo = (TextView) view.findViewById(R.id.sources);
 		Linkify.addLinks(sourcesInfo, Linkify.WEB_URLS);
 
 		return view;
+	}
+
+	private void sendFeedback(Context context, String version) {
+		final Intent emailIntent = new Intent(Intent.ACTION_SENDTO,
+				Uri.fromParts("mailto", "iliakplv@gmail.com", null));
+		final String subject = context.getString(R.string.app_name) + " feedback";
+		emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+		emailIntent.putExtra(Intent.EXTRA_TEXT, getTextForFeedback(context));
+		startActivity(Intent.createChooser(emailIntent, context.getString(R.string.about_contact)));
+	}
+
+	private String getTextForFeedback(Context context) {
+		return context.getString(R.string.about_contact_do_not_remove) + Utils.getDeviceInformation();
 	}
 
 	public static void show(FragmentManager fm) {
