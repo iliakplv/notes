d5f71946df44f4aad710b60a994016e7e5833537
iliakplv <iliakplv@gmail.com>
2016-06-14 21:56:34 +1200

    Add resources

---
+++ app/src/main/java/com/iliakplv/notes/NotesApplication.java
@@ -0,0 +1,61 @@
+package com.iliakplv.notes;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.iliakplv.notes.notes.dropbox.DropboxHelper;
+import com.iliakplv.notes.notes.storage.Storage;
+import com.iliakplv.notes.utils.AppLog;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class NotesApplication extends Application {
+
+    private static final String TAG = NotesApplication.class.getSimpleName();
+    private static Context context;
+
+    private static final int NON_CORE_THREADS_KEEP_ALIVE_TIME_SECONDS = 1;
+    private static ThreadPoolExecutor executor;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppLog.d(TAG, "onCreate() call");
+
+        context = getApplicationContext();
+        initThreadPool();
+        initStorage();
+    }
+
+    private void initStorage() {
+        Storage.init(null);
+        DropboxHelper.initSynchronization();
+    }
+
+    private void initThreadPool() {
+        final int processors = Runtime.getRuntime().availableProcessors();
+        AppLog.d(TAG, "Detected " + processors + " processors. Creating thread pool...");
+        executor = new ThreadPoolExecutor(processors,
+                processors,
+                NON_CORE_THREADS_KEEP_ALIVE_TIME_SECONDS,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>());
+    }
+
+    @Override
+    public void onTerminate() {
+        AppLog.d(TAG, "onTerminate() call");
+        super.onTerminate();
+    }
+
+
+    public static Context getContext() {
+        return context;
+    }
+
+    public static void executeInBackground(Runnable task) {
+        executor.execute(task);
+    }
+}
